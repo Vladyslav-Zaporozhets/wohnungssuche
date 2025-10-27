223ce4f666b71e92ba5cc4024e9a587c863c489e
Vladyslav Zaporozhets <ziper.step@gmail.com>
2025-10-27 18:27:14 +0100

    Initial project setup with Vite

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 // --- Головний файл (Точка входу) ---
 
 // 1. Імпортуємо всі наші CSS файли
-import "./css/main.css";
-import "./css/layout.css";
-import "./css/sections.css";
+import "../css/main.css";
+import "../css/layout.css";
+import "../css/sections.css";
 
 // 2. Імпортуємо нашу логіку програми
 import { initApp } from "./app.js";
